3b2d6986cdbba5d9d34f275b7603e3946890e121
Midecodes <faginorish@gmail.com>
2025-04-20 00:19:46 +0100

    remove unused states and functions

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Header from "components/components/header";
 import Siderbar from "components/components/sidebar";
 import React from "react";
@@ -8,24 +7,13 @@ const DashboardLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  React.useEffect(() => {
-    const isMobile = typeof window !== "undefined" && window.innerWidth <= 768;
-    if (!isMobile) {
-      setIsOpen(false);
-    }
-  }, []);
   return (
     <div className="dashboard-layout">
       <div className="flex">
-        <Siderbar {...{ isOpen, toggleSidebar }} />
+        <Siderbar />
 
         <main className="flex flex-col w-full">
-          <Header {...{ toggleSidebar }} />
+          <Header />
           {children}
         </main>
       </div>
